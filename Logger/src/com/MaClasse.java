37bca2134a7faa9f1d027bc8874d16f6a8ec723d
Alexandre Mlanao Amadi <mlanaoamadi.alexandre@gmail.com>
2015-02-24 19:14:32 +0100

    Use the Factory to get object of concrete class

--- Logger/src/com/MaClasse.java
+++ Logger/src/com/MaClasse.java
@@ -3,9 +3,27 @@ package com;
 public class MaClasse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
+		
+		LoggerFactory loggerFactory = new LoggerFactory();
+		
+		//Get an object of Debug mode and call its display method
+		Logger logger1 = loggerFactory.getLogger("debug");
+		
+		//Call the display method of Debug
+		logger1.display();
+		
+		//Get an object of Info mode and call its display method
+		Logger logger2 = loggerFactory.getLogger("info");
+		
+		//Call the display method of Info
+		logger2.display();
+		
+		//Get an object of Error mode and call its display method
+		Logger logger3 = loggerFactory.getLogger("error");
+		
+		//Call the display method of Error
+		logger3.display();
+	
 	}
 
 }
