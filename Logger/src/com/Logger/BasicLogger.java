5256398919610f02f4672df8ad403e6393038cb3
Nicolas Moreau <nicolasmoreau78@gmail.com>
2015-02-25 21:55:18 +0100

    Création de la classe BasicLogger qui va regrouper les 3 logger déja présent.

---
+++ Logger/src/com/Logger/BasicLogger.java
@@ -0,0 +1,27 @@
+package com.Logger;
+
+public class BasicLogger implements Logger{
+
+
+	
+	@Override
+	public void debug(String message) {
+		// TODO Auto-generated method stub
+	
+	}
+
+
+	@Override
+	public void info(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void error(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
