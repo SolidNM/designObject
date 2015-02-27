1f6a89b3e6fbe43e2e962047da6d5d35bb8c7fa8
Nicolas Moreau <nicolasmoreau78@gmail.com>
2015-02-27 11:19:21 +0100

    Ajout de commentaires dans la classe LoggerFactory.

--- Logger/src/com/Logger/LoggerFactory.java
+++ Logger/src/com/Logger/LoggerFactory.java
@@ -12,16 +12,19 @@ public class LoggerFactory {
 		
 		if(loggerType.equalsIgnoreCase("DEBUG")){
 			PrintStream out;
+			//Remplacer System.out par votre cible pour le logger DEBUG.
 			out = new PrintStream(System.out);
 			return new BasicLogger(LevelOfLog.DEBUG, out);
 		
 		}else if(loggerType.equalsIgnoreCase("INFO")){
 			PrintStream out;
+			//Remplacer System.out par votre cible pour le logger INFO.
 			out = new PrintStream(System.out);
 			return new BasicLogger(LevelOfLog.INFO, out);
 			
 		}else if(loggerType.equalsIgnoreCase("ERROR")){
 			PrintStream out;
+			//Remplacer System.out par votre cible pour le logger ERROR.
 				out = new PrintStream(System.out);
 				return new BasicLogger(LevelOfLog.ERROR, out);
 			
